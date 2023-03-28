a03186b70b641ad259585f5c17b83fabc39bd0b3
Muhiddin Kurbonov <muhiddindc@gmail.com>
2023-03-28 15:17:56 -0500

    created db on mongodb atlas, set up db connection in app.js using mongoose

--- app.js
+++ app.js
@@ -1,9 +1,24 @@
 require("dotenv").config();
 const express = require("express");
+const mongoose = require("mongoose");
 
 const app = express();
 
 const PORT = process.env.PORT || 8000;
+
+const uri = process.env.MONGODB_URL;
+
+mongoose
+  .connect(uri, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => {
+    console.log("Connected to MongoDB");
+  })
+  .catch((error) => {
+    console.error("Error connecting to MongoDB:", error.message);
+  });
 
 app.get("/test", (req, res) => {
   res.send("Test");
